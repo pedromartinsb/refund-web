9263b3f972b10f89e28ba60ed0dd30dcf51d9b88
Pedro Campos <engpedrocampos@gmail.com>
2026-01-06 10:56:07 -0300

    feat(manager-router): loading component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,18 @@ import { BrowserRouter } from "react-router";
 import { AuthRoutes } from "./AuthRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
+import { Loading } from "../components/Loading";
+
+const isLoading = false;
 
 export function Routes() {
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <BrowserRouter>
-      <ManagerRoutes />
+      <AuthRoutes />
     </BrowserRouter>
   );
 }
